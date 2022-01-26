18717afbd22e342411d9b747dbf6addc3e01cfea
Tinh Cao <caotinh98c@gmail.com>
2022-01-26 20:47:51 +0700

    Remove unused library

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ import requests
 import os
 # For dealing with json responses we receive from the API
 import json
-# For displaying the data after
-import pandas as pd
 # For saving the response data in CSV format
 import csv
 # For parsing the dates received from twitter in readable formats
